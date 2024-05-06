fa02052e45c81de4e04b9e5b9de969760340207c
Mykola Savenko <mykola.savenko21@gmail.com>
2024-05-06 17:15:29 -0500

    feat: add basepath and remove devtools from prod

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,7 +1,16 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router';
-import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 
 import { Header } from './-components/header';
+import React from 'react';
+
+const TanStackRouterDevtools =
+  import.meta.env.NODE_ENV === 'production'
+    ? () => null
+    : React.lazy(() =>
+        import('@tanstack/router-devtools').then((res) => ({
+          default: res.TanStackRouterDevtools,
+        })),
+      )
 
 export const Route = createRootRoute({
   component: () => (
