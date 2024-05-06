695b26995215994a23330ffec88b0c25a9446006
Mykola Savenko <mykola.savenko21@gmail.com>
2024-05-06 17:02:35 -0500

    feat: add path

--- assets/index.lazy-CXuJ-DF6.js
+++
@@ -1,1 +0,0 @@
-import{c as e,j as o}from"./index-LnsL56Om.js";const n=e("/off-road/")({component:t});function t(){return o.jsx("div",{children:o.jsx("h3",{children:"Off Road!"})})}export{n as Route};
