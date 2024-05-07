ce300085f0b9992c6cf4fd21792ff9e646ea4fec
Mykola Savenko <mykola.savenko21@gmail.com>
2024-05-06 22:43:52 -0500

    try another vite plugin

---
+++ assets/index.lazy-yFKnjBuo.js
@@ -0,0 +1,1 @@
+import{c as e,j as o}from"./index-CWHtcSx4.js";const n=e("/off-road/")({component:t});function t(){return o.jsx("div",{children:o.jsx("h3",{children:"Off Road!"})})}export{n as Route};
